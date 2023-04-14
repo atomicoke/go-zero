c30b9747a4dfd67fdcde3973355d1c4af5c481d4
fzdwx <likelovec@gmail.com>
2023-04-14 14:53:32 +0800

    Update `update` item type

--- tools/goctl/curd/memberutil.go
+++ tools/goctl/curd/memberutil.go
@@ -1,6 +1,7 @@
 package curd
 
 import (
+	"github.com/iancoleman/strcase"
 	"github.com/zeromicro/go-zero/tools/goctl/api/spec"
 	"github.com/zeromicro/go-zero/tools/goctl/model/sql/parser"
 )
@@ -27,4 +28,45 @@ func membersAndPk(members []spec.Member, pk *parser.Primary) []spec.Member {
 
 func emptyMembers(members []spec.Member, pk *parser.Primary) []spec.Member {
 	return []spec.Member{}
+}
+
+func pageReqMembers(members []spec.Member, pk *parser.Primary) []spec.Member {
+	return append([]spec.Member{
+		{
+			Name: "Page",
+			Type: spec.PrimitiveType{RawName: "int64"},
+			Tag:  mapFormTagWithValid("page", "页码", "number,gte=1"),
+		},
+		{
+			Name: "Limit",
+			Type: spec.PrimitiveType{RawName: "int64"},
+			Tag:  mapFormTagWithValid("limit", "每页数量", "number,gte=1,lte=100"),
+		},
+	}, members...)
+}
+
+func pageRespItemsMembers(members []spec.Member, pk *parser.Primary) []spec.Member {
+	return membersAndPk(members, pk)
+}
+
+func pageRespMembers(prefix string) ([]spec.Member, string) {
+	prefix = strcase.ToCamel(prefix)
+	name := prefix + "PageItem"
+	return append([]spec.Member{
+		{
+			Name: "Page",
+			Type: spec.PrimitiveType{RawName: "int64"},
+			Tag:  mapJsonTag("page", ""),
+		},
+		{
+			Name: "Limit",
+			Type: spec.PrimitiveType{RawName: "int64"},
+			Tag:  mapJsonTag("limit", "每页数量"),
+		},
+		{
+			Name: "Total",
+			Type: spec.PrimitiveType{RawName: "int64"},
+			Tag:  mapJsonTag("total", "总数"),
+		},
+	}, spec.Member{Name: "List", Type: spec.ArrayType{RawName: "[]" + name}, Tag: mapJsonTag("list", "列表数据")}), name
 }
